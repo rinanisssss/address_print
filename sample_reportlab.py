08869c9be1250d09a82400aff90a6e419335406e
rinanisssss <rina.nishizaki0124mafu@gmail.com>
2023-07-15 17:11:03 +0900

    postal code printing done

---
+++ sample_reportlab.py
@@ -0,0 +1,10 @@
+from reportlab.pdfgen import canvas
+
+c = canvas.Canvas("output_file/hello.pdf")
+c.setFont("Courier", 80)
+c.drawString(100, 100, "Hello World")
+c.showPage()
+c.setFontSize(40)
+c.drawString(100, 100, "Hello World")
+c.showPage()
+c.save()
